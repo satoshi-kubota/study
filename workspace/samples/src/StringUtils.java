3790a76c2bb698921a80504507acf53ea0539f3a
久保田哲 <notfound.pan@gmail.com>
2013-07-21 18:09:13 +0900

    クロージャとの比較

---
+++ workspace/samples/src/StringUtils.java
@@ -0,0 +1,17 @@
+
+public class StringUtils {
+
+	public static boolean isBlank(String str){
+		
+		if(str == null || str.length() == 0){
+			return true;
+		}
+		
+		for(int i=0;i<str.length();i++){
+			if(!Character.isWhitespace(str.charAt(i))){
+				return false;
+			}
+		}
+		return true;
+	}
+}
